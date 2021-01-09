f17ec470c2df28b63fa4bb12a5a35fd0a89b8675
JakeL1001 <jakelyell@iinet.net.au>
2021-01-09 12:00:30 +0800

    update laptop

--- KovaaKs Graph Generator.py
+++ KovaaKs Graph Generator.py
@@ -1,26 +1,60 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
+from tkinter import *
+from tkinter import ttk
 import plotly.express as px
 import plotly.graph_objects as go
+import pandas as pd
 
-templocation = "C:\\Users\\jakee\\Desktop\\Kovaaks\\"
-tempinput = "Cata_IC_Long_Strafes.csv"
+mypath = "C:\\Users\\jakee\\Desktop\\Kovaaks\\"
 
-scenario = templocation + tempinput
-print(scenario)
+from os import listdir
+from os.path import isfile, join
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
+OPTIONS = onlyfiles
+class MyFirstGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("TEST :)")
 
-thing = "C:\\Users\\jakee\\Desktop\\Kovaaks\\gp_far_long_strafes.csv"
-thing = scenario
-#thing = input("pog")
-def create_graph(filepath):
-    df = pd.read_csv(filepath)
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(y=df['Score'],
+        self.label = Label(master, text="This is our first GUI!")
+        #self.label.pack()
+
+        self.generate_button = Button(master, text="Generate", command=self.generate)
+        #self.greet_button.pack()
+
+        self.variable = StringVar(master)
+        self.variable.set(OPTIONS[0])
+        self.selection = ttk.Combobox(master, textvariable=self.variable, values=OPTIONS)
+        #self.selection.pack()
+
+        self.ok_button = Button(master, text="OK", command=self.ok)
+        #self.ok_button.pack()
+
+        self.close_button = Button(master, text="Close", command=master.quit)
+        #self.close_button.pack()
+
+        self.label.grid(columnspan=2, sticky=W)
+        self.generate_button.grid(row=1)
+        self.ok_button.grid(row=1, column=1)
+        self.selection.grid(rowspan=2, row=1)
+        self.close_button.grid(row=2, column=1)
+
+    def generate(self):
+        location = "C:\\Users\\jakee\\Desktop\\Kovaaks\\"
+        file = self.variable.get()
+        filepath = location + file
+        df = pd.read_csv(filepath)
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(y=df['Score'],
                         mode='lines+markers',
                         name='lines+markers'))
-    fig.update_xaxes(rangeslider_visible=True)
-    fig.show()
-create_graph(thing)
+        fig.update_xaxes(rangeslider_visible=True)
+        fig.show()
 
+    def ok(self):
+        print ("value is: " + self.variable.get())
+
+root = Tk()
+root.geometry("300x300")
+my_gui = MyFirstGUI(root)
+root.mainloop()
